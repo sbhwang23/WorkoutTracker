311da634ab881e74d8993d6dbce0ed6b03eeb179
Sarah <soobin.sarah.hwang@gmail.com>
2020-10-16 20:29:49 -0700

    initial commitment

---
+++ models/workout.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        exercises: [
+            {
+                name: {
+                    type: String,
+                    trim: true,
+                    required: 'Enter the name of exercise.'
+                },
+                type: {
+                    type: String,
+                    trim: true,
+                    required: 'Enter the type of exercise.'
+                },
+                duration: {
+                    type: Number,
+                    required: 'Enter the duration of exercise in minutes'
+                },
+                weight: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                distanceTraveled: {
+                    type: Number
+                }
+            }
+        ]
+    });
+
+// workoutSchema.virtual('totalDuration').get(function() {
+//     return this.exercises.reduce((total, exercise) => {
+//         return total + exercise.duration;
+//     }, 0);
+// });
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Transaction;
---
+++ routes/apiRoutes.js
@@ -0,0 +1,45 @@
+const Workout = require('../models/workout');
+module.exports = function(app){
+    app.get('/api/workouts', function(req, res) {
+        Workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
+
+    app.post('/api/workouts', function (req, res) {
+        Workout.create({})
+        .then(data => {res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
+
+    app.get('api/workouts/range', function (req,res) {
+        Workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
+
+    app.post('/api/workouts/:id', ({body, params}, res) => {
+        Workout.findByIDAndUpdate(
+            params.id,
+            {$push:{exercises:body}},
+            {new: true, runValidators: true}
+        )
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
+}
